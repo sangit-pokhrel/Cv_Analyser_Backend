6782300841ef68dcc0bf3a0bcf0d176bc36eee0a
sangit-pokhrel <heypsycho339@gmail.com>
2025-11-22 22:07:49 +0545

    Completed Register and Login page

--- src/app/(site)/Register/page.tsx
+++ src/app/(site)/Register/page.tsx
@@ -1,15 +1,14 @@
-import Image from "next/image";
-import "../globals/styles/style.color.css"
 import Registerpage from "@/components/Registerpage_component";
 import { Metadata } from "next";
-
 export const metadata: Metadata = {
   title: "Register | Cv Saathi",
   description: "Create an account on Cv Saathi to access personalized job recommendations, application tracking, and career resources tailored to your profile.",
   icons: "/globe.svg",
 };
-export default function Home() {
-  return (
-    <Registerpage/>
-  );
+
+
+const  Register=()=> {
+  return <Registerpage/>;
 }
+
+export default Register;
