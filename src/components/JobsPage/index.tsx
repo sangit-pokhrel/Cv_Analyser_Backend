0ada8daac888ae716b7bdaa41ac55ec531ccc3e2
Sangit Pokhrel <heypsycho339@gmail.com>
2025-12-01 21:29:02 +0545

    Fixed case rename

--- src/components/JobsPage/index.tsx
+++ src/components/JobsPage/index.tsx
@@ -5,7 +5,7 @@ import { IoBagCheckOutline } from "react-icons/io5";
 import HeroSection from "./heroSection";
 import BrowseByCategory from "./browseByCategory";
 import FullWidthContainer from "@/globals/fullwidthcontainer";
-import WhySection from "./whySection";
+import WhySection from "./WhySection";
 import FeaturedJobsSec from "./featuredJobsSec";
 const JobsPage=()=>{
   return (  
