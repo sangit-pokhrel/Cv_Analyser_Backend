1974157b85a133c6be7a6d56b4468580822045dc
Sangit Pokrel ( Sikeee ) <108231114+sangit-pokhrel@users.noreply.github.com>
2026-01-11 17:06:59 +0545

    some api link changes

--- src/components/Loginpage_component/api/loginApi.ts
+++ src/components/Loginpage_component/api/loginApi.ts
@@ -11,7 +11,7 @@ function getCookie(name: string) {
 }
  
 const LoginMutationFunc =async (data:any)=>{
-  const res = await axios.post('http://localhost:5000/api/v1/auth/login',data, {
+  const res = await axios.post('https://cv-analyser-backend.onrender.com',data, {
     headers: {
       "Content-Type": "application/json",
       
